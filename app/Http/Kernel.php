7c6ef6cd25d9c63952e7d4ffe3f9e2d3478381b5
Atsushi Okui <okui@motionpicture.jp>
2020-07-03 16:46:00 +0900

    Change SubstituteBindings to Doctrine

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -36,12 +36,12 @@ class Kernel extends HttpKernel
             // \Illuminate\Session\Middleware\AuthenticateSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \LaravelDoctrine\ORM\Middleware\SubstituteBindings::class,
         ],
 
         'api' => [
             'throttle:60,1',
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \LaravelDoctrine\ORM\Middleware\SubstituteBindings::class,
         ],
     ];
 
@@ -55,7 +55,7 @@ class Kernel extends HttpKernel
     protected $routeMiddleware = [
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
-        'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        'bindings' => \LaravelDoctrine\ORM\Middleware\SubstituteBindings::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
