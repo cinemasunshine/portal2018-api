894e9548f2ea3bf91aad007535049683f38c1249
Atsushi Okui <okui@motionpicture.jp>
2020-07-07 21:35:45 +0900

    Fix phpcs errors

--- app/Providers/DoctrineServiceProvider.php
+++ app/Providers/DoctrineServiceProvider.php
@@ -25,7 +25,7 @@ class DoctrineServiceProvider extends ServiceProvider
      */
     public function boot(CacheManager $cacheManager)
     {
-        $cacheManager->extend('wincache', function() {
+        $cacheManager->extend('wincache', function () {
             return new WinCacheCache();
         });
     }
