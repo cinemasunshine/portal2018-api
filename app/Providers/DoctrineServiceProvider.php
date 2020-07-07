7399a37c8841d6980c3426cfc3683257f4ad690e
Atsushi Okui <okui@motionpicture.jp>
2020-07-07 20:48:02 +0900

    Add Doctrine WInCache driver

---
+++ app/Providers/DoctrineServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Providers;
+
+use Doctrine\Common\Cache\WinCacheCache;
+use Illuminate\Support\ServiceProvider;
+use LaravelDoctrine\ORM\Configuration\Cache\CacheManager;
+
+class DoctrineServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot(CacheManager $cacheManager)
+    {
+        $cacheManager->extend('wincache', function() {
+            return new WinCacheCache();
+        });
+    }
+}
