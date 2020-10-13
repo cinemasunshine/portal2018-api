62ed72a1568762f493e30208ffab5997e7edbe94
Atsushi Okui <okui@motionpicture.jp>
2020-10-13 18:32:26 +0900

    Added database factories

---
+++ database/factories/ShowingTheaterFactory.php
@@ -0,0 +1,10 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Doctrine\Entities\ShowingTheater;
+use Faker\Generator as Faker;
+
+$factory->define(ShowingTheater::class, function (Faker $faker) {
+    return [];
+});
