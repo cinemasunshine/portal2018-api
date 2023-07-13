b415d4a7966b278bff44292bdcb2c0348b4766b3
Atsushi Okui <okui@motionpicture.jp>
2023-07-13 12:05:02 +0900

    feat!: 上映情報の未使用のフィールドを削除
    - 上映方式を示す `fromats`
    - 上映劇場を示す `theaters`

--- tests/Unit/Http/Resources/ScheduleTest.php
+++ tests/Unit/Http/Resources/ScheduleTest.php
@@ -7,13 +7,9 @@ namespace Tests\Unit\Http\Resources;
 use App\Doctrine\Entities\Schedule as ScheduleEntity;
 use App\Doctrine\Entities\Title as TitleEntity;
 use App\Http\Resources\Schedule;
-use App\Http\Resources\ShowingFormat;
-use App\Http\Resources\ShowingTheater;
 use App\Http\Resources\Title;
 use DateTime;
-use Doctrine\Common\Collections\ArrayCollection;
 use Illuminate\Http\Request;
-use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 use Mockery;
 use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
 use Mockery\LegacyMockInterface;
@@ -56,13 +52,11 @@ class ScheduleTest extends TestCase
      */
     public function testToArray(): void
     {
-        $id              = 5;
-        $startDate       = '2020-07-10';
-        $endDate         = '2020-07-31';
-        $remark          = 'example remark';
-        $title           = new TitleEntity();
-        $showingFormats  = new ArrayCollection();
-        $showingTheaters = new ArrayCollection();
+        $id        = 5;
+        $startDate = '2020-07-10';
+        $endDate   = '2020-07-31';
+        $remark    = 'example remark';
+        $title     = new TitleEntity();
 
         $scheduleEntityMock = $this->createScheduleEntityMock();
         $scheduleEntityMock
@@ -85,30 +79,6 @@ class ScheduleTest extends TestCase
             ->shouldReceive('getTitle')
             ->once()
             ->andReturn($title);
-        $scheduleEntityMock
-            ->shouldReceive('getShowingFormats')
-            ->once()
-            ->andReturn($showingFormats);
-        $scheduleEntityMock
-            ->shouldReceive('getShowingTheaters')
-            ->once()
-            ->andReturn($showingTheaters);
-
-        $formats           = Mockery::mock(AnonymousResourceCollection::class);
-        $showingFormatMock = $this->createShowingFormatMock();
-        $showingFormatMock
-            ->shouldReceive('collection')
-            ->once()
-            ->with(Mockery::type('array'))
-            ->andReturn($formats);
-
-        $theaters           = Mockery::mock(AnonymousResourceCollection::class);
-        $showingTheaterMock = $this->createShowingTheaterMock();
-        $showingTheaterMock
-            ->shouldReceive('collection')
-            ->once()
-            ->with(Mockery::type('array'))
-            ->andReturn($theaters);
 
         $targetMock = $this->createTargetMock();
         $targetMock->makePartial();
@@ -123,23 +93,5 @@ class ScheduleTest extends TestCase
         $this->assertEquals($endDate, $result['end_date']);
         $this->assertEquals($remark, $result['remark']);
         $this->assertInstanceOf(Title::class, $result['title']);
-        $this->assertEquals($formats, $result['formats']);
-        $this->assertEquals($theaters, $result['theaters']);
-    }
-
-    /**
-     * @return MockInterface&LegacyMockInterface&ShowingFormat
-     */
-    protected function createShowingFormatMock()
-    {
-        return Mockery::mock('alias:' . ShowingFormat::class);
-    }
-
-    /**
-     * @return MockInterface&LegacyMockInterface&ShowingTheater
-     */
-    protected function createShowingTheaterMock()
-    {
-        return Mockery::mock('alias:' . ShowingTheater::class);
     }
 }
