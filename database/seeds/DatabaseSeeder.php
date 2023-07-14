2556e394c557ca48bf807db657871ea7e227878d
Atsushi Okui <okui@motionpicture.jp>
2023-07-14 12:56:16 +0900

    fix: デプロイに失敗するのでseederを戻す

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+    }
+}
